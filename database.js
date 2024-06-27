401707cc48fb1c19b27f5a067383727424a7a1f4
Suraj-Razor <14635@coderacademy.edu.au>
2024-06-27 20:07:58 +1000

    Updated env

--- database.js
+++ database.js
@@ -1,25 +1,29 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const mongoose = require("mongoose");
 
-async function databaseConnect(){
-	let databaseURL = process.env.DATABASE_URL || "mongodb://localhost:27017/oct23-blog-db";
+async function databaseConnect() {
+    let databaseURL =
+        process.env.DATABASE_URL || "mongodb://localhost:27017/oct23-blog-db";
 
-	await mongoose.connect(databaseURL);
-	console.log("Database connecting completed!");
+    await mongoose.connect(databaseURL);
+    console.log("Database connecting completed!");
 }
 
-async function databaseClose(){
-	// Disconnect from database 
-	await mongoose.connection.close();
-	console.log("DB is disconnected!");
+async function databaseClose() {
+    // Disconnect from database
+    await mongoose.connection.close();
+    console.log("DB is disconnected!");
 }
 
-async function databaseClear(){
-	// Delete all data in database
-	await mongoose.connection.db.dropDatabase();
+async function databaseClear() {
+    // Delete all data in database
+    await mongoose.connection.db.dropDatabase();
 }
 
 module.exports = {
-	databaseConnect,
-	databaseClose,
-	databaseClear
-}
+    databaseConnect,
+    databaseClose,
+    databaseClear
+};
